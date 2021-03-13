e1d02643f06c59cbb0830ca7d270e4f58b901e69
PushpakB3096 <rtpushpak@gmail.com>
2021-03-13 17:08:12 +0530

    Add ChatEngine component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { ChatEngine } from "react-chat-engine";
+
+import "./assets/App.css";
+
+const App = () => {
+  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+};
+
+export default App;
