a1f215445547301aafda21b42005f0eca380c5eb
PushpakB3096 <rtpushpak@gmail.com>
2021-03-13 17:18:17 +0530

    Use chat engine credentials to create room

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { ChatEngine } from "react-chat-engine";
 import "./assets/App.css";
 
 const App = () => {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  return (
+    <ChatEngine
+      height="100vh"
+      userName="PushpakB"
+      userSecret="pass123"
+      projectID="4cd623f3-87bf-4ea8-ad0e-a72a0783cece"
+    />
+  );
 };
 
 export default App;
